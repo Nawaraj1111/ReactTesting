25123182f50aa4a1414e55dd83041cb651e902b8
Pro <luitel.nawaraj12300@gmail.com>
2025-10-04 18:36:44 +0545

    Fun is added

--- src/Fun.js
+++ src/Fun.js
@@ -4,7 +4,7 @@ function Fun() {
     return (
         <div>
             <h1>Welcome to My App</h1>
-            <p>This is a basic React application.</p>
+            <p>This is a basic React application. fun is added </p>
         </div>
     );
 }
