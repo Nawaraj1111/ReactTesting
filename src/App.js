d1e2d2b2763d04447e3e33837e4bf5e7de2fc731
Pro <luitel.nawaraj12300@gmail.com>
2025-10-04 18:35:43 +0545

    Fun is just added to check deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 // import { BrowserRouter } from 'react-router-dom';
 import React,{ Suspense,lazy } from 'react';
 import Form from './Form';
+import Fun from './Fun';
 import Practice from './Practice';
 
 const Contact = lazy(() => import('./Contact'));
 
 
 function App() {
-  const myValue = {
-    color:"green",
-    backgroundColor: "black",
-    padding:"10px",
-    fontfamily:"Sans-Serif"
-  }
 
   return (
     <div style={{myValue}}>
       <Suspense fallback={<p>loading.....</p>}>
        <Contact />
        <Form />
+       <Fun />
       <Practice />
       </Suspense>
     </div>
---
+++ src/Fun.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Fun() {
+    return (
+        <div>
+            <h1>Welcome to My App</h1>
+            <p>This is a basic React application.</p>
+        </div>
+    );
+}
+
+export default Fun;
